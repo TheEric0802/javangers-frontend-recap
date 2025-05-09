809868cda0b6daed6e1a29fe21a76e048ec73d24
Eric Schmidt <eric0802@web.de>
2025-05-09 12:44:35 +0200

    fetch and render Todos from backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,29 @@
 import './App.css'
 import Header from "./Header.tsx";
 import TodoPage from "./TodoPage.tsx";
+import {useCallback, useEffect, useState} from "react";
+import type {Todo} from "./types/Todo.ts";
+import axios from "axios";
 
 function App() {
+    const [Todos, setTodos] = useState<Todo[]>([])
+
+    const getTodos = useCallback(() => {
+        axios.get<Todo[]>("/api/todo")
+            .then(response => {
+                setTodos(response.data)
+            })
+    }, [])
+
+    useEffect(() => {
+        getTodos()
+        }, [getTodos]
+    )
 
   return (
     <>
         <Header />
-        <TodoPage />
+        <TodoPage todos={Todos}/>
     </>
   )
 }
--- src/TodoPage.tsx
+++ src/TodoPage.tsx
@@ -1,20 +1,36 @@
 import TodoContainer from "./TodoContainer.tsx";
 import "./TodoPage.css"
 import TodoCard from "./TodoCard.tsx";
+import type {Todo} from "./types/Todo.ts";
 
-export default function TodoPage() {
+type TodoPageProps = {
+    todos: Todo[]
+}
+
+export default function TodoPage(props: TodoPageProps) {
+
+    const openTodoCards = props.todos.filter((todo) => todo.status === "OPEN").map((todo) => (
+        <TodoCard key={todo.id} todo={todo} />
+    ))
+    const inProgressTodoCards = props.todos.filter((todo) => todo.status === "IN_PROGRESS").map((todo) => (
+        <TodoCard key={todo.id} todo={todo} />
+    ))
+    const doneTodoCards = props.todos.filter((todo) => todo.status === "DONE").map((todo) => (
+        <TodoCard key={todo.id} todo={todo} />
+    ))
+
     return (
         <>
             <button>+ Add task</button>
             <div className={"lists"}>
                 <TodoContainer heading={"OPEN"}>
-                    <TodoCard todo={{id: "1", description: "test", status: "OPEN"}} />
+                    {openTodoCards}
                 </TodoContainer>
                 <TodoContainer heading={"IN PROGRESS"}>
-                    <p>test child</p>
+                    {inProgressTodoCards}
                 </TodoContainer>
                 <TodoContainer heading={"DONE"}>
-                    <p>test child</p>
+                    {doneTodoCards}
                 </TodoContainer>
             </div>
         </>
